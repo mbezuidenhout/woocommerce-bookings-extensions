7cfffe991c91e963e2c619086a834615b5f09581
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2019-07-15 17:53:41 +0200

    - Finished hotfix

--- includes/class-wc-bookings-extensions.php
+++ includes/class-wc-bookings-extensions.php
@@ -205,7 +205,7 @@ class WC_Bookings_Extensions {
 		$this->loader->add_action( 'woocommerce_admin_booking_data_after_booking_details', $plugin_admin, 'add_admin_booking_fields' );
 		$this->loader->add_action( 'save_post', $plugin_admin, 'save_meta_box', 10, 2 );
 
-		$this->loader->add_action( 'plugins_loaded', $plugin_admin, 'load_extensions', 12 );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'change_calendar', 12 );
 	}
 
 	/**
