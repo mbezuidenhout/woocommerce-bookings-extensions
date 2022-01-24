d337d26a01fb6e8f6efb156318246329e5ccd595
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2022-01-24 17:44:11 +0200

    Updated version and compatibility parameters

--- woocommerce-bookings-extensions.php
+++ woocommerce-bookings-extensions.php
@@ -14,7 +14,7 @@
  * Plugin Name:          WooCommerce Bookings Extensions
  * Plugin URI:           https://github.com/mbezuidenhout/woocommerce-bookings-extensions
  * Description:          Adds or replaces functionality in the WooCommerce Bookings plugin.
- * Version:              1.9.3
+ * Version:              1.9.4
  * Requires PHP:         7.4
  * Requires at least:    5.0
  * Tested up to:         5.8.3
@@ -24,7 +24,7 @@
  * License URI:          http://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:          woocommerce-bookings-extensions
  * Domain Path:          /languages
- * WC tested up to:      6.0.0
+ * WC tested up to:      6.1.1
  * WC requires at least: 5.4
  */
 
@@ -38,7 +38,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WOOCOMMERCE_BOOKINGS_EXTENSIONS_VERSION', '1.9.3' );
+define( 'WOOCOMMERCE_BOOKINGS_EXTENSIONS_VERSION', '1.9.4' );
 
 /**
  * The code that runs during plugin activation.
